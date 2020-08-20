7108ca5e8fc635c852af4942f87e2ddd58d6a84a
Himanshu Mishra <54707793+HimanshuMishir@users.noreply.github.com>
2020-08-20 08:45:32 +0530

    Update tables & data

--- classdb.sql
+++ classdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 15, 2020 at 07:22 AM
+-- Generation Time: Aug 20, 2020 at 04:53 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.2.32
 
@@ -29,26 +29,28 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `standards` (
   `t_id` int(11) NOT NULL,
-  `1` varchar(11) NOT NULL,
-  `2` varchar(11) NOT NULL,
-  `3` varchar(11) NOT NULL,
-  `4` varchar(11) NOT NULL,
-  `5` varchar(11) NOT NULL,
-  `6` varchar(11) NOT NULL,
-  `7` varchar(11) NOT NULL,
-  `8` varchar(11) NOT NULL,
-  `9` varchar(11) NOT NULL,
-  `10` varchar(11) NOT NULL,
-  `11` varchar(11) NOT NULL,
-  `12` varchar(11) NOT NULL
+  `s1` varchar(2) NOT NULL,
+  `s2` varchar(2) NOT NULL,
+  `s3` varchar(2) NOT NULL,
+  `s4` varchar(2) NOT NULL,
+  `s5` varchar(2) NOT NULL,
+  `s6` varchar(2) NOT NULL,
+  `s7` varchar(2) NOT NULL,
+  `s8` varchar(2) NOT NULL,
+  `s9` varchar(2) NOT NULL,
+  `s10` varchar(2) NOT NULL,
+  `s11` varchar(2) NOT NULL,
+  `s12` varchar(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `standards`
 --
 
-INSERT INTO `standards` (`t_id`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `11`, `12`) VALUES
-(5, '', '', '', '', '', '', '', '', '', '10', '11', '12');
+INSERT INTO `standards` (`t_id`, `s1`, `s2`, `s3`, `s4`, `s5`, `s6`, `s7`, `s8`, `s9`, `s10`, `s11`, `s12`) VALUES
+(11, '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'),
+(12, '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'),
+(21, '', '', '3', '4', '5', '', '', '', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -57,23 +59,27 @@ INSERT INTO `standards` (`t_id`, `1`, `2`, `3`, `4`, `5`, `6`, `7`, `8`, `9`, `1
 --
 
 CREATE TABLE `students` (
-  `student_id` int(5) NOT NULL,
-  `name` varchar(100) NOT NULL,
+  `s_id` int(11) NOT NULL,
+  `roll` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
   `gender` varchar(6) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `phone` varchar(200) NOT NULL,
-  `std` varchar(10) NOT NULL
+  `phone` bigint(10) NOT NULL,
+  `std` varchar(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`student_id`, `name`, `gender`, `email`, `phone`, `std`) VALUES
-(35, 'Laali', 'Female', 'laali@mail.com', '1231231231', '12'),
-(36, 'Shivam Singh', 'Male', 'shiva@mail.com', '1213211231', '12'),
-(37, 'Himanshu Mishra', 'Male', 'hm12378900@gmail.com', '9506005060', '12'),
-(38, 'Ashutosh Tripathi', 'Male', 'ashu@mail.com', '1231241241', '1');
+INSERT INTO `students` (`s_id`, `roll`, `name`, `gender`, `email`, `phone`, `std`) VALUES
+(1, 1, 'Aarav Mishra', 'Male', 'aarav@mail.com', 0, '1'),
+(4, 2, 'A B', 'Male', '12321@1.c', 9955995500, '12'),
+(6, 22, 'Mohit', 'Male', 'moh@mail.com', 9898983749, '8'),
+(7, 10, 'Shreya', 'Female', 'sh@mail.com', 8398398494, '10'),
+(8, 9, 'Manavi MIshra', 'Female', 'manavi@mail.com', 8282832984, '12'),
+(9, 7, 'Anmol Singh', 'Male', 'anmol@mail.com', 7898372383, '3'),
+(10, 4, 'Annanya Pandey', 'Female', 'anni@mail.com', 8487293829, '11');
 
 -- --------------------------------------------------------
 
@@ -82,22 +88,31 @@ INSERT INTO `students` (`student_id`, `name`, `gender`, `email`, `phone`, `std`)
 --
 
 CREATE TABLE `subjects` (
-  `t_id` int(11) NOT NULL,
-  `std` varchar(50) NOT NULL,
-  `Hindi` varchar(50) NOT NULL,
-  `English` varchar(50) NOT NULL,
-  `Marathi` varchar(50) NOT NULL,
-  `Science` varchar(50) NOT NULL,
-  `Math` varchar(50) NOT NULL,
-  `Social_Studies` varchar(50) NOT NULL
+  `t_id` int(11) DEFAULT NULL,
+  `s_id` int(11) DEFAULT NULL,
+  `sub1` varchar(20) NOT NULL,
+  `sub2` varchar(20) NOT NULL,
+  `sub3` varchar(20) NOT NULL,
+  `sub4` varchar(20) NOT NULL,
+  `sub5` varchar(20) NOT NULL,
+  `sub6` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `subjects`
 --
 
-INSERT INTO `subjects` (`t_id`, `std`, `Hindi`, `English`, `Marathi`, `Science`, `Math`, `Social_Studies`) VALUES
-(5, '12', '', '', '', 'Science', 'Math', '');
+INSERT INTO `subjects` (`t_id`, `s_id`, `sub1`, `sub2`, `sub3`, `sub4`, `sub5`, `sub6`) VALUES
+(11, NULL, 'Hindi', 'English', 'Marathi', 'Math', 'Science', 'Social Studies'),
+(NULL, 1, 'Hindi', 'English', 'Marathi', 'Math', 'Science', 'Social Studies'),
+(12, NULL, 'Hindi', 'English', 'Marathi', 'Math', 'Science', 'Social Studies'),
+(21, NULL, '', '', 'Marathi', 'Math', '', ''),
+(NULL, 6, 'Hindi', 'English', '', 'Math', 'Science', ''),
+(NULL, 4, '', '', '', '', 'Science', 'Social Studies'),
+(NULL, 7, 'Hindi', 'English', 'Marathi', 'Math', '', ''),
+(NULL, 8, 'Hindi', 'English', 'Marathi', 'Math', 'Science', ''),
+(NULL, 10, 'Hindi', 'English', '', 'Math', 'Science', 'Social Studies'),
+(NULL, 9, '', '', 'Marathi', 'Math', 'Science', '');
 
 -- --------------------------------------------------------
 
@@ -106,26 +121,24 @@ INSERT INTO `subjects` (`t_id`, `std`, `Hindi`, `English`, `Marathi`, `Science`,
 --
 
 CREATE TABLE `teachers` (
-  `t_id` int(3) NOT NULL,
-  `t_name` varchar(100) NOT NULL,
+  `t_id` int(11) NOT NULL,
+  `t_name` varchar(50) NOT NULL,
   `gender` varchar(6) NOT NULL,
   `email` varchar(50) NOT NULL,
   `phone` bigint(20) NOT NULL,
-  `qualification` varchar(100) NOT NULL,
-  `experience` int(11) NOT NULL,
-  `std` varchar(100) NOT NULL,
-  `sub` varchar(100) NOT NULL
+  `qualification` varchar(50) NOT NULL,
+  `experience` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `teachers`
 --
 
-INSERT INTO `teachers` (`t_id`, `t_name`, `gender`, `email`, `phone`, `qualification`, `experience`, `std`, `sub`) VALUES
-(4, 'Julie', 'Female', 'julie@1.1', 9026043020, 'B.Tech', 100, '1,2,3,4,5,6,7,8', 'Hindi,English,Marathi'),
-(5, 'Himanshu mishra', 'Male', 'hm8216414@gmail.com', 9506005060, 'B.Tech', 10, '10,11,12', 'Math,Science'),
-(7, 'Himanshu Mishra', 'Male', 'hm12378900@gmail.com', 9506005060, 'B.Tech', 3, '12', 'Science'),
-(8, 'Jenny', 'Female', '12321@1.c', 9955995500, 'B.Sc', 12, '12', 'Math,Science');
+INSERT INTO `teachers` (`t_id`, `t_name`, `gender`, `email`, `phone`, `qualification`, `experience`) VALUES
+(5, 'Rajesh Kumar', 'Male', 'rajesh@mail.com', 9323231212, 'B.Tech', 8),
+(11, 'Neha Singh', 'Female', 'neha@mail.com', 1213211231, 'B.com', 5),
+(12, 'Ashutosh', 'Male', 'ashu@mail.com', 9323231212, 'B.com', 5),
+(21, 'Akhand', 'Male', 'shiva@mail.com', 8373283231, 'B.A.', 10);
 
 --
 -- Indexes for dumped tables
@@ -135,19 +148,21 @@ INSERT INTO `teachers` (`t_id`, `t_name`, `gender`, `email`, `phone`, `qualifica
 -- Indexes for table `standards`
 --
 ALTER TABLE `standards`
-  ADD PRIMARY KEY (`t_id`);
+  ADD UNIQUE KEY `t_id` (`t_id`);
 
 --
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
-  ADD PRIMARY KEY (`student_id`);
+  ADD PRIMARY KEY (`s_id`),
+  ADD UNIQUE KEY `roll` (`roll`);
 
 --
 -- Indexes for table `subjects`
 --
 ALTER TABLE `subjects`
-  ADD PRIMARY KEY (`t_id`);
+  ADD UNIQUE KEY `t_id` (`t_id`),
+  ADD UNIQUE KEY `s_id` (`s_id`);
 
 --
 -- Indexes for table `teachers`
@@ -163,13 +178,24 @@ ALTER TABLE `teachers`
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `student_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `s_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT for table `teachers`
+-- Constraints for dumped tables
 --
-ALTER TABLE `teachers`
-  MODIFY `t_id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for table `standards`
+--
+ALTER TABLE `standards`
+  ADD CONSTRAINT `standards_ibfk_1` FOREIGN KEY (`t_id`) REFERENCES `teachers` (`t_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `subjects`
+--
+ALTER TABLE `subjects`
+  ADD CONSTRAINT `subjects_ibfk_1` FOREIGN KEY (`t_id`) REFERENCES `teachers` (`t_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `subjects_ibfk_2` FOREIGN KEY (`s_id`) REFERENCES `students` (`s_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
